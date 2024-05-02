84f2a8b0c3fa3617a439aa8c095072a07d6eb953
ZiyadRW <zeadalrouasheede@gmail.com>
2024-05-02 19:39:10 +0300

    V3.1

--- MyScript.js
+++ MyScript.js
@@ -13,7 +13,7 @@
 })();
 
 function openPdf(){
-    var pdfPath = './img/ZeadAlrouasheedResume.pdf'; 
+    var pdfPath = './img/Alruwaished-updatedCV.pdf'; 
 
     // Open the PDF file in a new tab
     window.open(pdfPath, '_blank');
